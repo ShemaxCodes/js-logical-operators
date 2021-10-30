a7c26ce63bcb5f326c724fe1d29d41ee05c74880
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-10-30 09:56:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 /*
 Logical Operators
 */
+
+const hasDriversLicense = true; // A
+const hasGoodVision = false; // B
+
+console.log(hasDriversLicense && hasGoodVision); // AND
+console.log(hasDriversLicense || hasGoodVision); // OR
+console.log(!hasDriversLicense); // NOT
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if(hasDriversLicense && hasGoodVision) {
+    console.log('Sarah is able to drive!'); // this only runs if condition is true
+} else {
+    console.log('Someone else should drive...');
+}
